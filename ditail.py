57e1a4b56cf413e2efa662406fd2c31e8885d716
eisin <eisin@users.noreply.github.com>
2018-05-20 17:46:44 +0900

    Update ditail.py

--- ditail.py
+++ ditail.py
@@ -7,13 +7,14 @@ import asyncio
 import logging
 import os
 import sys
+import re
 
 import pyinotify
 
 logger = logging.getLogger("ditail")
 
 @asyncio.coroutine
-def tail_task(path, modify_event, loop, new=False):
+def tail_task(path, modify_event, loop, disable_truncate_detect, new=False):
     f = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
     try:
         if not new:
@@ -23,19 +24,20 @@ def tail_task(path, modify_event, loop, new=False):
         guard_truncate = False
         line_buffer = bytearray()
         while True:
-            block = os.read(f, 4096)
+            block = os.read(f, 100 * 1024 * 1024)
             if block == b'' and guard_truncate:
                 # Reset so os.read will return data again
                 logger.debug("Truncation detected for {}, resetting".format(path))
-                os.lseek(f, 0, os.SEEK_DATA)
-                block = os.read(f, 4096)
+                os.lseek(f, 0, os.SEEK_SET)
+                block = os.read(f, 100 * 1024 * 1024)
             guard_truncate = False
 
             if block == b'':
                 logger.debug("No more to read for {}, waiting for modify event".format(path))
                 yield from modify_event.wait()
                 modify_event.clear()
-                guard_truncate = True
+                if not disable_truncate_detect:
+                    guard_truncate = True
             else:
                 line_buffer.extend(block)
                 while True:
@@ -44,16 +46,22 @@ def tail_task(path, modify_event, loop, new=False):
                         break
                     else:
                         # Write out through newline, remove that portion from buffer
+                        sys.stdout.write(path.encode())
+                        sys.stdout.write(b":")
                         sys.stdout.write(line_buffer[:(n+1)])
                         line_buffer = line_buffer[(n+1):]
     finally:
         os.close(f)
 
-def update_tasks_in(directory, tail_tasks, tail_events, loop):
+def update_tasks_in(directory, tail_tasks, tail_events, loop, filter_regexp, disable_truncate_detect):
     wm = pyinotify.WatchManager()
     mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
 
     class Handler(pyinotify.ProcessEvent):
+        def __init__(self, filter_regexp, disable_truncate_detect):
+            self.filter_regexp = filter_regexp
+            self.disable_truncate_detect = disable_truncate_detect
+
         def process_IN_CREATE(self, event):
             path = event.pathname
             if os.path.isfile(path):
@@ -61,10 +69,15 @@ def update_tasks_in(directory, tail_tasks, tail_events, loop):
                     logger.debug("Cancelling existing task for {}".format(path))
                     tail_tasks[path].cancel()
                     del tail_tasks[path]
-                logger.debug("Creating task for {}".format(path))
-                modify_event = asyncio.Event(loop=loop)
-                tail_events[path] = modify_event
-                tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop, new=True))
+                if self.filter_regexp:
+                    pattern = re.compile(self.filter_regexp)
+                if not filter_regexp or pattern.search(path):
+                    logger.debug("Creating task for {}".format(path))
+                    modify_event = asyncio.Event(loop=loop)
+                    tail_events[path] = modify_event
+                    tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop, self.disable_truncate_detect, new=True))
+                else:
+                    logger.debug("New path {} not matched the filter, ignoring".format(path))
             else:
                 logger.debug("New path {} is not a file, ignoring".format(path))
 
@@ -85,27 +98,30 @@ def update_tasks_in(directory, tail_tasks, tail_events, loop):
             else:
                 logger.debug("No event to set for {}".format(path))
 
-    notifier = pyinotify.AsyncioNotifier(wm, loop, default_proc_fun=Handler())
+    notifier = pyinotify.AsyncioNotifier(wm, loop, default_proc_fun=Handler(filter_regexp, disable_truncate_detect))
     wm.add_watch(directory, mask, rec=True, auto_add=True)
 
-def generate_files_in(directory):
+def generate_files_in(directory, filter_regexp):
+    if filter_regexp:
+        pattern = re.compile(filter_regexp)
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            yield os.path.join(dirpath, filename)
+            if not filter_regexp or pattern.search(filename):
+                yield os.path.join(dirpath, filename)
 
-def tail_files_in(directory):
+def tail_files_in(directory, filter_regexp, disable_truncate_detect):
     loop = asyncio.get_event_loop()
     tail_tasks = dict()
     tail_events = dict()
     abs_directory = os.path.abspath(directory)
 
     try:
-        for path in generate_files_in(abs_directory):
+        for path in generate_files_in(abs_directory, filter_regexp):
             logger.debug("Creating initial task for {}".format(path))
             modify_event = asyncio.Event(loop=loop)
             tail_events[path] = modify_event
-            tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop))
-        update_tasks_in(abs_directory, tail_tasks, tail_events, loop)
+            tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop, disable_truncate_detect))
+        update_tasks_in(abs_directory, tail_tasks, tail_events, loop, filter_regexp, disable_truncate_detect)
 
         logger.debug("Starting loop")
         loop.run_forever()
@@ -129,6 +145,8 @@ def tail_files_in(directory):
 def create_argparser():
     parser = argparse.ArgumentParser(description="Prints new lines added to all files in a directory and its subdirectories. Any files added after startup will also be monitored.")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable logging (at DEBUG level)")
+    parser.add_argument("-f", "--filter", action="store", required=False, help="Filter filename (regexp)")
+    parser.add_argument("--disable-truncate-detect", action="store_true", help="Disable file truncate detection")
     parser.add_argument("directory", help="The directory to look in for files to tail")
     return parser
 
@@ -144,7 +162,7 @@ def main():
     if args.debug:
         setup_logging()
     configure_stdio()
-    tail_files_in(args.directory)
+    tail_files_in(args.directory, args.filter, args.disable_truncate_detect)
 
 if __name__ == "__main__":
     main()

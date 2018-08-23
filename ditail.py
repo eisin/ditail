a2b8a4a681e38bb87c1895714b04f12b14cbd29a
eisin <eisin@users.noreply.github.com>
2018-08-23 23:50:20 +0900

    Add feature that excludes files not recently modified

--- ditail.py
+++ ditail.py
@@ -8,6 +8,7 @@ import logging
 import os
 import sys
 import re
+import datetime
 
 import pyinotify
 
@@ -53,13 +54,14 @@ def tail_task(path, modify_event, loop, disable_truncate_detect, new=False):
     finally:
         os.close(f)
 
-def update_tasks_in(directory, tail_tasks, tail_events, loop, filter_regexp, disable_truncate_detect):
+def update_tasks_in(directory, tail_tasks, tail_events, loop, filter_regexp, mmin, disable_truncate_detect):
     wm = pyinotify.WatchManager()
     mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
 
     class Handler(pyinotify.ProcessEvent):
-        def __init__(self, filter_regexp, disable_truncate_detect):
+        def __init__(self, filter_regexp, mmin, disable_truncate_detect):
             self.filter_regexp = filter_regexp
+            self.mmin = mmin
             self.disable_truncate_detect = disable_truncate_detect
 
         def process_IN_CREATE(self, event):
@@ -71,13 +73,13 @@ def update_tasks_in(directory, tail_tasks, tail_events, loop, filter_regexp, dis
                     del tail_tasks[path]
                 if self.filter_regexp:
                     pattern = re.compile(self.filter_regexp)
-                if not filter_regexp or pattern.search(path):
+                if self.filter_regexp and not pattern.search(path):
+                    logger.debug("New path {} not matched the filter, ignoring".format(path))
+                else:
                     logger.debug("Creating task for {}".format(path))
                     modify_event = asyncio.Event(loop=loop)
                     tail_events[path] = modify_event
                     tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop, self.disable_truncate_detect, new=True))
-                else:
-                    logger.debug("New path {} not matched the filter, ignoring".format(path))
             else:
                 logger.debug("New path {} is not a file, ignoring".format(path))
 
@@ -98,30 +100,36 @@ def update_tasks_in(directory, tail_tasks, tail_events, loop, filter_regexp, dis
             else:
                 logger.debug("No event to set for {}".format(path))
 
-    notifier = pyinotify.AsyncioNotifier(wm, loop, default_proc_fun=Handler(filter_regexp, disable_truncate_detect))
+    notifier = pyinotify.AsyncioNotifier(wm, loop, default_proc_fun=Handler(filter_regexp, mmin, disable_truncate_detect))
     wm.add_watch(directory, mask, rec=True, auto_add=True)
 
-def generate_files_in(directory, filter_regexp):
+def generate_files_in(directory, filter_regexp, mmin):
     if filter_regexp:
         pattern = re.compile(filter_regexp)
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            if not filter_regexp or pattern.search(filename):
-                yield os.path.join(dirpath, filename)
+            fullpath = os.path.join(dirpath, filename)
+            if filter_regexp and pattern.search(filename):
+                continue
+            if mmin and not mmin_filter_allow(fullpath, mmin):
+                continue
+            yield fullpath
 
-def tail_files_in(directory, filter_regexp, disable_truncate_detect):
+def tail_files_in(directory, filter_regexp, mmin, disable_truncate_detect):
     loop = asyncio.get_event_loop()
     tail_tasks = dict()
     tail_events = dict()
     abs_directory = os.path.abspath(directory)
 
     try:
-        for path in generate_files_in(abs_directory, filter_regexp):
+        for path in generate_files_in(abs_directory, filter_regexp, mmin):
             logger.debug("Creating initial task for {}".format(path))
             modify_event = asyncio.Event(loop=loop)
             tail_events[path] = modify_event
             tail_tasks[path] = loop.create_task(tail_task(path, modify_event, loop, disable_truncate_detect))
-        update_tasks_in(abs_directory, tail_tasks, tail_events, loop, filter_regexp, disable_truncate_detect)
+        update_tasks_in(abs_directory, tail_tasks, tail_events, loop, filter_regexp, mmin, disable_truncate_detect)
+        if mmin and mmin > 0:
+            loop.call_soon(watch_mmin_task, loop, tail_tasks, mmin)
 
         logger.debug("Starting loop")
         loop.run_forever()
@@ -138,6 +146,30 @@ def tail_files_in(directory, filter_regexp, disable_truncate_detect):
             pass
         loop.close()
 
+def watch_mmin_task(loop, tail_tasks, mmin):
+    del_tasks = []
+    for path in tail_tasks:
+        if not mmin_filter_allow(path, mmin):
+            logger.debug("Cancelling existing task for {}, because not recently modified".format(path))
+            del_tasks.append(path)
+    for path in del_tasks:
+        tail_tasks[path].cancel()
+        del tail_tasks[path]
+    loop.call_later(60, watch_mmin_task, loop, tail_tasks, mmin)
+
+def mmin_filter_allow(filename, mmin):
+    if mmin == 0:
+        return True
+    try:
+        mtime = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
+    except:
+        pass
+    mtime_diff = datetime.datetime.now() - mtime
+    mtime_diff_minutes = int(mtime_diff.total_seconds() / 60)
+    if mmin >= mtime_diff_minutes:
+        return True
+    return False
+
 #
 # Main
 #
@@ -145,7 +177,8 @@ def tail_files_in(directory, filter_regexp, disable_truncate_detect):
 def create_argparser():
     parser = argparse.ArgumentParser(description="Prints new lines added to all files in a directory and its subdirectories. Any files added after startup will also be monitored.")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable logging (at DEBUG level)")
-    parser.add_argument("-f", "--filter", action="store", required=False, help="Filter filename (regexp)")
+    parser.add_argument("-f", "--filter", action="store", help="Filter filename (regexp)")
+    parser.add_argument("-m", "--mmin", action="store", type=int, help="Exclude files not recently modified (in minutes)")
     parser.add_argument("--disable-truncate-detect", action="store_true", help="Disable file truncate detection")
     parser.add_argument("directory", help="The directory to look in for files to tail")
     return parser
@@ -162,7 +195,7 @@ def main():
     if args.debug:
         setup_logging()
     configure_stdio()
-    tail_files_in(args.directory, args.filter, args.disable_truncate_detect)
+    tail_files_in(args.directory, args.filter, args.mmin, args.disable_truncate_detect)
 
 if __name__ == "__main__":
     main()
